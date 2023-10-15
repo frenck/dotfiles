725b51160b6e0846e8b2938f04598442c0db460e
Franck Nijhof <git@frenck.dev>
2023-10-15 10:43:40 +0200

    Add nvim files

---
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+-- Shades of purple is my all time favorite theme anywhere!
+return {
+  { "Rigellute/shades-of-purple.vim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "shades_of_purple",
+    },
+  },
+}
