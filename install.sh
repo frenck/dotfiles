ed39255dd5857eea7980908fbad568614ffc4c36
Franck Nijhof <git@frenck.dev>
2023-10-16 23:18:28 +0200

    MOAR fixes

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ if ! command -v curl >/dev/null; then
 	exit 1
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+bin_dir="${HOME}/.local/bin"
 
 # Install brew on macOS
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -21,13 +21,18 @@ if [[ "$(uname)" == "Darwin" ]]; then
 	brew install cmake coreutils git
 fi
 
+# Install on needed packages on Linux
+if [[ "$(uname)" == "Linux" ]]; then
+	sudo apt update
+	sudo apt install -y build-essential gettext
+fi
+
 if ! chezmoi="$(command -v chezmoi)"; then
-	bin_dir="${HOME}/.local/bin"
 	chezmoi="${bin_dir}/chezmoi"
 	echo "Installing chezmoi to '${chezmoi}'" >&2
 	chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
 	sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-	unset chezmoi_install_script bin_dir
+	unset chezmoi_install_script
 fi
 
 echo "Running chezmoi" >&2
@@ -37,15 +42,18 @@ ${chezmoi} init --apply --source="${script_dir}"
 if [[ "$(uname)" == "Darwin" ]]; then
 	brew install rtx
 elif [[ "${CODESPACES:-}" == "true" ]]; then
-	curl https://rtx.pub/rtx-latest-linux-x64 >"${HOME}/.local/bin/rtx"
-	chmod a+x "${HOME}/.local/bin/rtx"
+	curl https://rtx.pub/rtx-latest-linux-x64 >"${bin_dir}/rtx"
+	chmod a+x "${bin_dir}/rtx"
+	rtx="${bin_dir}/rtx"
 else
 	sudo install -dm 755 /etc/apt/keyrings
 	wget -qO - https://rtx.pub/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/rtx-archive-keyring.gpg 1>/dev/null
 	echo "deb [signed-by=/etc/apt/keyrings/rtx-archive-keyring.gpg arch=amd64] https://rtx.pub/deb stable main" | sudo tee /etc/apt/sources.list.d/rtx.list
 	sudo apt update
 	sudo apt install -y rtx
+	rtx="rtx"
 fi
-rtx trust ~/.config/rtx/config.toml
-rtx install --yes
 
+echo "Running rtx" >&2
+${rtx} trust ~/.config/rtx/config.toml
+${rtx} install --yes
